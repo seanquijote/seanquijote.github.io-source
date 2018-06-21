d3f3b690c2a9e5ac21934fd17a2f9ee5179cdf85
sknqt <seanquijote@gmail.com>
2018-06-21 15:32:35 +0800

    Add router component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ Title ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.router.ts
@@ -0,0 +1,28 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+// import { Name3Component } from './';
+// import { Name4Component } from './';
+// import { PageNotFoundComponent } from './';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: '**', component: AppComponent }
+
+    // { path: 'path4', component: Name4Component },
+    // { path: 'path/:routeParam', component: MyComponent },
+    // { path: 'staticPath', component: ... },
+    // { path: '**', component: ... },
+    // { path: 'oldPath', redirectTo: '/staticPath' },
+    // { path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
