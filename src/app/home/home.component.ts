87c1b6c485d41ba8b722c16ec4f7278f113afce1
seanquijote <sean@nextixsystems.com>
2020-02-29 22:13:39 +0800

    Remove angular-particle and angular-image-gallery

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,7 +24,6 @@ export class HomeComponent implements OnInit, OnDestroy {
 	constructor(@Inject(WINDOW) private window: Window, private router: Router, private route: ActivatedRoute) {}
 
 	ngOnInit() {
-    console.log("onInit")
     this.typedOptions = {
       strings: [
         "jr. software engineer",
@@ -45,118 +44,112 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.typedDesktop = new Typed('.typed-element-desktop', this.typedOptions);
     this.typedMobile = new Typed('.typed-element-mobile', this.typedOptions);
 
-    this.particleStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
-      'z-index': -1,
-      'top': 0,
-      'left': 0,
-    }
+    // this.particleStyle = {
+    //   'position': 'fixed',
+    //   'width': '100%',
+    //   'height': '100%',
+    //   'z-index': -1,
+    //   'top': 0,
+    //   'left': 0,
+    // }
 
-    this.particleParams = {
-      particles: {
-        number: {
-          value: 50,
-          density: {
-            enable: true,
-            value_area: 800
-          }
-        },
-        color: { value: "#eeeeee" },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#000000"
-          },
-          polygon: { nb_sides: 5 }
-        },
-        opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false
-          }
-        },
-        size: {
-          value: 1,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false
-          }
-        },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#eeeeee",
-          opacity: 0.4,
-          width: 1
-        },
-        move: {
-          enable: true,
-          speed: 6,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
-          }
-        }
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: false,
-            mode: "repulse"
-          },
-          onclick: {
-            enable: false,
-            mode: "push"
-          },
-          resize: true
-        },
-        modes: {
-          grab: {
-            distance: 200,
-            line_linked: { opacity: 1 }
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8,
-            speed: 3
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4
-          },
-          push: { particles_nb: 4 },
-          remove: { particles_nb: 2 }
-        }
-      },
-      retina_detect: true
-    }
+    // this.particleParams = {
+    //   particles: {
+    //     number: {
+    //       value: 50,
+    //       density: {
+    //         enable: true,
+    //         value_area: 800
+    //       }
+    //     },
+    //     color: { value: "#eeeeee" },
+    //     shape: {
+    //       type: "circle",
+    //       stroke: {
+    //         width: 0,
+    //         color: "#000000"
+    //       },
+    //       polygon: { nb_sides: 5 }
+    //     },
+    //     opacity: {
+    //       value: 0.5,
+    //       random: false,
+    //       anim: {
+    //         enable: false,
+    //         speed: 1,
+    //         opacity_min: 0.1,
+    //         sync: false
+    //       }
+    //     },
+    //     size: {
+    //       value: 1,
+    //       random: true,
+    //       anim: {
+    //         enable: false,
+    //         speed: 40,
+    //         size_min: 0.1,
+    //         sync: false
+    //       }
+    //     },
+    //     line_linked: {
+    //       enable: true,
+    //       distance: 150,
+    //       color: "#eeeeee",
+    //       opacity: 0.4,
+    //       width: 1
+    //     },
+    //     move: {
+    //       enable: true,
+    //       speed: 6,
+    //       direction: "none",
+    //       random: false,
+    //       straight: false,
+    //       out_mode: "out",
+    //       bounce: false,
+    //       attract: {
+    //         enable: false,
+    //         rotateX: 600,
+    //         rotateY: 1200
+    //       }
+    //     }
+    //   },
+    //   interactivity: {
+    //     detect_on: "canvas",
+    //     events: {
+    //       onhover: {
+    //         enable: false,
+    //         mode: "repulse"
+    //       },
+    //       onclick: {
+    //         enable: false,
+    //         mode: "push"
+    //       },
+    //       resize: true
+    //     },
+    //     modes: {
+    //       grab: {
+    //         distance: 200,
+    //         line_linked: { opacity: 1 }
+    //       },
+    //       bubble: {
+    //         distance: 400,
+    //         size: 40,
+    //         duration: 2,
+    //         opacity: 8,
+    //         speed: 3
+    //       },
+    //       repulse: {
+    //         distance: 200,
+    //         duration: 0.4
+    //       },
+    //       push: { particles_nb: 4 },
+    //       remove: { particles_nb: 2 }
+    //     }
+    //   },
+    //   retina_detect: true
+    // }
   }
 
   ngOnDestroy() {
-    console.log("onDestroy")
-    this.typedOptions = null
-    this.typedDesktop = null
-    this.typedMobile = null
-    this.particleStyle = null
-    this.particleParams = null
   }
 }
